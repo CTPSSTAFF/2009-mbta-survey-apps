43260c2adea3e6a9ab2077a463e3f1397a90db2a
Ben Krepp <bkrepp@ctps.org>
2018-08-22 15:35:33 -0400

    JS utilities for rapid transit survey results app.

---
+++ appUtils.js
@@ -0,0 +1,18 @@
+var appUtils = {};
+
+// All MBTA rapid transit stations--surface Green Line stations grouped as in survey.
+appUtils.aTransitStations = ['Alewife','Davis','Porter','Harvard','Central','Kendall/MIT','Charles/MGH','Park Street (R)','Downtown Crossing (R)','South Station',
+					'Broadway','Andrew','JFK/UMass','Savin Hill','Fields Corner','Shawmut','Ashmont (R)','North Quincy','Wollaston','Quincy Center',
+					'Quincy Adams','Braintree','Lechmere','Science Park','North Station (G)','Haymarket (G)','Government Center (G)','Park Street (G)','Boylston','Arlington',
+					'Copley','Hynes Convention Ctr/ICA','Kenmore','Prudential','Symphony','Oak Grove','Malden Center','Wellington','Sullivan Square','Community College',
+					'North Station (O)','Haymarket (O)','State (O)','Downtown Crossing (O)','Chinatown','NE Medical Center','Back Bay','Massachusetts Ave','Ruggles','Roxbury Crossing',
+					'Jackson Square','Stony Brook','Green Street','Forest Hills','Wonderland','Revere Beach','Beachmont','Suffolk Downs','Orient Heights','Wood Island',
+					'Airport','Maverick','Aquarium','State (B)','Government Center (B)','Bowdoin','Cedar Grove','Butler','Milton','Central Avenue',
+					'Valley Road','Capen Street','Mattapan','Ashmont (M)',
+					'B: Blandford to Babcock','B: Packards Cnr to Warren','B: Washington to Boston College',
+					'C: St. Marys to Summit/Winchester','C: Brandon to Cleveland Cir',
+					'D: Fenway to Longwood','D: Brookline Village to Brookline Hills','D: Beaconsfield to Chestnut Hill',
+					'D: Newton Ctr to Eliot','D: Waban to Riverside',
+					'E: Northeastern to Museum of Fine Arts','E: Longwood Medical Area to Brigham Cir','E: Fenwood to Heath'
+					];
+ // appUtils.aTransitStations
